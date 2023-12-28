99f98fa1d966f90d797306ac42ade07fa01f7eea
Bryce Campbell <bryceacampbell7@gmail.com>
2023-12-27 17:16:42 -0700

    add cache control header

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,6 +1,7 @@
 import { getSequelize } from "@/sequelize";
 
-export async function GET(request: Request) {
+export async function GET(request: Request, response: Response) {
+  response.headers.set("Cache-Control", "no-store");
   const sequelize = await getSequelize();
   const [results] = await sequelize.query("SELECT * FROM test.test");
   return Response.json(results);
