abf8fb089e827bc807d68092b45c86a1018cf25f
Bryce Campbell <bryceacampbell7@gmail.com>
2023-12-27 17:56:11 -0700

    add error handling

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,14 +3,24 @@ import { getSequelize } from "@/sequelize";
 export const maxDuration = 300;
 
 export async function POST() {
-  const sequelize = await getSequelize();
-  const [results] = await sequelize.query("SELECT * FROM test.test");
-  return Response.json(results, {
-    headers: {
-      "Cache-Control": "no-cache",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "POST",
-      "Access-Control-Allow-Headers": "Content-Type",
-    },
-  });
+  try {
+    const sequelize = await getSequelize();
+    const [results] = await sequelize.query("SELECT * FROM test.test");
+    return Response.json(results, {
+      headers: {
+        "Cache-Control": "no-cache",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return Response.json(
+      { error: "An error occurred" },
+      {
+        status: 500,
+      }
+    );
+  }
 }
