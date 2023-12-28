a61848d79fa66f30df47bc6270ac909b947a8bda
Bryce Campbell <bryceacampbell7@gmail.com>
2023-12-27 19:44:50 -0700

    reset

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -7,15 +7,7 @@ export async function POST() {
   try {
     const sequelize = await getSequelize();
     const [results] = await sequelize.query("SELECT * FROM test.test");
-    return NextResponse.json(results, {
-      status: 200,
-      headers: {
-        "Cache-Control": "no-cache, no-store, must-revalidate",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      },
-    });
+    return NextResponse.json(results);
   } catch (err) {
     console.error(err);
     return NextResponse.json(
