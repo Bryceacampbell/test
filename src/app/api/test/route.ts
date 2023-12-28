fa7befcd5638187ec2f9e0e26b1508276f543090
Bryce Campbell <bryceacampbell7@gmail.com>
2023-12-27 17:50:20 -0700

    add cors headers

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -8,6 +8,9 @@ export async function POST() {
   return Response.json(results, {
     headers: {
       "Cache-Control": "no-cache",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST",
+      "Access-Control-Allow-Headers": "Content-Type",
     },
   });
 }
