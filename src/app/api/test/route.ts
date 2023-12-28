c5f5c456acc5af358972b8a008b4372ebea3106d
Bryce Campbell <bryceacampbell7@gmail.com>
2023-12-27 17:28:56 -0700

    change to post, add loading test

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,6 +1,6 @@
 import { getSequelize } from "@/sequelize";
 
-export async function GET() {
+export async function POST() {
   const sequelize = await getSequelize();
   const [results] = await sequelize.query("SELECT * FROM test.test");
   return Response.json(results, {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [data, setData] = useState(null);
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/test`)
+    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/test`, {
+      method: "POST",
+    })
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
 
-  return data && JSON.stringify(data);
+  return data ? JSON.stringify(data) : "Loading...";
 }
