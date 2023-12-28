7a907040e978c3f86cdcfc53e7ae68c7299b6427
Bryce Campbell <bryceacampbell7@gmail.com>
2023-12-28 13:34:47 -0700

    display perf metrics

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -6,7 +6,9 @@ export const maxDuration = 300;
 export async function GET() {
   try {
     const sequelize = await getSequelize();
-    const [results] = await sequelize.query("SELECT * FROM test.test");
+    const [results] = await sequelize.query(
+      "SELECT * FROM test.test WHERE study_id='9'"
+    );
     return NextResponse.json(results);
   } catch (err) {
     console.error(err);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,46 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [data, setData] = useState(null);
+  const [metrics, setMetrics] = useState<any[]>([]);
+
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/test`, {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((data) => setData(data));
+    const fetchData = async () => {
+      const promises = Array.from({
+        length: Number(process.env.NEXT_PUBLIC_REQUEST_COUNT),
+      }).map(async (_, i) => {
+        const start = performance.now();
+        await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/test`, {
+          method: "GET",
+        });
+        const end = performance.now();
+        return (end - start) / 1000;
+      });
+
+      const times = await Promise.all(promises);
+      setMetrics(times);
+    };
+
+    fetchData();
   }, []);
 
-  return data ? JSON.stringify(data) : "Loading...";
+  return (
+    <div>
+      {metrics.length ? (
+        <>
+          <h1>Performance Metrics</h1>
+          {metrics.map((metric, index) => (
+            <p key={index}>
+              Request {index + 1}: {metric.toFixed(2)} s
+            </p>
+          ))}
+          <p>
+            Average Time:{" "}
+            {(metrics.reduce((a, b) => a + b, 0) / metrics.length).toFixed(2)} s
+          </p>
+        </>
+      ) : (
+        "Loading..."
+      )}
+    </div>
+  );
 }
